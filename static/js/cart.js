95f9af456ae9f7900cc4da89cb01153bb29307ab
ABDULRAHMAN <business.zinon@gmail.com>
2021-03-14 13:21:49 +0200

    test-5

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,14 +1,15 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
 for (var i = 0; i <updateBtns.length; i++) {
-    updateBtns[i].addEventListener('click', function () {
+    updateBtns[i].addEventListener('click', function() {
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
+
         console.log('USER:', user)
-        if (user == 'AnonymousUser'){
+        if (user === 'AnonymousUser'){
             console.log('Not logged in')
-        }else {
+        }else{
             updateUserOrder(productId, action)
         }
 
@@ -18,7 +19,9 @@ for (var i = 0; i <updateBtns.length; i++) {
 
 function updateUserOrder(productId, action) {
     console.log('User is logged in, sending data...')
+
     var url = '/update_item/'
+
     console.log('URL:', url)
 
     fetch(url,{
