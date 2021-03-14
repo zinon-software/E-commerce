1c13c1ac03fa23fde65222ae94863a6dc7619b0a
ABDULRAHMAN <business.zinon@gmail.com>
2021-03-14 11:08:54 +0200

    test-2

--- blog/views.py
+++ blog/views.py
@@ -91,3 +91,8 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse('Item was added', safe=False)
+
+
+def processOrder(request):
+
+    return JsonResponse('Payment submitted..', safe=False)
